ecbd5c8cea960a0a51b11aa98b4f557def17171f
Kylian Serrania <kylian.serrania@datadoghq.com>
2020-03-26 16:59:20 +0100

    Try x86 tests, builds with new builders

--- tasks/winbuildscripts/buildwin.bat
+++ tasks/winbuildscripts/buildwin.bat
@@ -25,18 +25,28 @@ if not exist \dev\go\src\github.com\DataDog\datadog-agent exit /b 1
 cd \dev\go\src\github.com\DataDog\datadog-agent || exit /b 2
 xcopy /e/s/h/q c:\mnt\*.* || exit /b 3
 
-inv -e deps --verbose --dep-vendor-only --no-checks || exit /b 4
+@echo GOPATH %GOPATH%
+SET PATH=%PATH%;%GOPATH%/bin
+@echo PATH %PATH%
+
+REM Equivalent to the "ridk enable" command, but without the exit
+@for /f %%p in ('where ridk') do set RIDK=%%p
+@for /f "delims=" %%x in ('"ruby" --disable-gems -x '%RIDK%' enable') do set "%%x"
+
+pip install -r requirements.txt || exit /b 4
+
+inv -e deps --verbose --dep-vendor-only --no-checks || exit /b 5
 
 @echo "inv -e %OMNIBUS_BUILD% %OMNIBUS_ARGS% --skip-deps --major-version %MAJOR_VERSION% --release-version %RELEASE_VERSION%"
-inv -e %OMNIBUS_BUILD% %OMNIBUS_ARGS% --skip-deps --major-version %MAJOR_VERSION% --release-version %RELEASE_VERSION% || exit /b 5
+inv -e %OMNIBUS_BUILD% %OMNIBUS_ARGS% --skip-deps --major-version %MAJOR_VERSION% --release-version %RELEASE_VERSION% || exit /b 6
 
 dir \omnibus\pkg
 
 dir \omnibus-ruby\pkg\
 
-if not exist %PKG_OUTDIR% mkdir %PKG_OUTDIR% || exit /b 6
-if exist \omnibus-ruby\pkg\*.msi copy \omnibus-ruby\pkg\*.msi %PKG_OUTDIR% || exit /b 7
-if exist \omnibus-ruby\pkg\*.zip copy \omnibus-ruby\pkg\*.zip %PKG_OUTDIR% || exit /b 8
+if not exist %PKG_OUTDIR% mkdir %PKG_OUTDIR% || exit /b 7
+if exist \omnibus-ruby\pkg\*.msi copy \omnibus-ruby\pkg\*.msi %PKG_OUTDIR% || exit /b 8
+if exist \omnibus-ruby\pkg\*.zip copy \omnibus-ruby\pkg\*.zip %PKG_OUTDIR% || exit /b 9
 
 goto :EOF
 
