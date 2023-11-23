efda4f71bdf1a48f090a91c477a37920bf7e9d2e
Sylvain Afchain <sylvain.afchain@datadoghq.com>
2023-11-23 10:01:24 +0100

    revert probe_linux

--- pkg/security/probe/probe_windows.go
+++ pkg/security/probe/probe_windows.go
@@ -217,4 +217,4 @@ func (p *WindowsProbe) FlushDiscarders() error {
 }
 
 // HandleActions executes the actions of a triggered rule
-func (p *WindowsProbe) andleActions(_ *rules.Rule, _ eval.Event) {}
+func (p *WindowsProbe) HandleActions(_ *rules.Rule, _ eval.Event) {}
